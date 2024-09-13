56a848078cce4a885fa4b7577309791887853e9c
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-13 17:06:42 -0300

    Update AssemblyInfo.cs with new informational version

--- StructuredProgramming/obj/Debug/net8.0/StructuredProgramming.AssemblyInfo.cs
+++ StructuredProgramming/obj/Debug/net8.0/StructuredProgramming.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StructuredProgramming")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdcde65736aa73a85da67397d3af432e8d43469d")]
 [assembly: System.Reflection.AssemblyProductAttribute("StructuredProgramming")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StructuredProgramming")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

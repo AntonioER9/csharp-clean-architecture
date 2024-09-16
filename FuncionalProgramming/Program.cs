233ac674562073ae3ac4febf399247445c6d20c6
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-16 15:56:08 -0300

    Refactor solution file to include FuncionalProgramming project

---
+++ FuncionalProgramming/Program.cs
@@ -0,0 +1,37 @@
+﻿// Función no pura
+DateTime Tomorrow()
+{
+  return DateTime.Now.AddDays(1);
+}
+
+Beer ToUpper(Beer beer)
+{
+  beer.Name = beer.Name.ToUpper();
+  return beer;
+}
+// Función pura
+DateTime TomorrowPure(DateTime date)
+{
+  return date.AddDays(1);
+}
+
+Beer ToUpperPure(Beer beer)
+{
+  var newBeer = new Beer() { Name = beer.Name.ToUpper() };
+  return newBeer;
+}
+
+var beer = new Beer() { Name = "Corona" };
+
+Console.WriteLine(ToUpperPure(beer).Name);
+Console.WriteLine(beer.Name);
+
+Console.WriteLine(Tomorrow());
+Console.WriteLine(TomorrowPure(DateTime.Now));
+
+
+
+public class Beer
+{
+  public string Name { get; set; }
+}

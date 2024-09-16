6ac51165f06271db065863fc0bf473b51dd0c8db
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-16 16:02:56 -0300

    Refactor Program.cs to include functional programming examples

--- FuncionalProgramming/Program.cs
+++ FuncionalProgramming/Program.cs
@@ -1,4 +1,25 @@
-﻿// Función no pura
+﻿var t = TomorrowPure;
+Console.WriteLine(t(new DateTime(2021, 1, 1)));
+
+Action<string> show = Console.WriteLine;
+show("Hello, World!"); // Hello, World!
+
+Action<string> hi = name => Console.WriteLine($"Hi, {name}!");
+hi("John"); // Hi, John!
+Action<int, int> add = (a, b) => Console.WriteLine(a + b);
+add(1, 2); // 3
+
+Func<int, int, int> sum = (a, b) => a + b;
+show(sum(1, 2).ToString()); // 3
+
+Func<int, int, string> mulString = (a, b) =>
+{
+  var result = a * b;
+  return result.ToString();
+};
+show(mulString(2, 3)); // 6
+
+// Función no pura
 DateTime Tomorrow()
 {
   return DateTime.Now.AddDays(1);

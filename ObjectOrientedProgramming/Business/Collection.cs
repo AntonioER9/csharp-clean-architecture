b4a074004f629284cf14f53cdcb4a6f6203dd06c
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-16 15:41:29 -0300

    Refactor Collection class to support generic elements and limit the collection size
    - Modify Collection class to be generic, allowing it to hold elements of any type
    - Add a constructor to Collection class that takes a limit parameter to set the maximum number of elements
    - Implement the Add method in Collection class to add elements to the collection, throwing an exception if the collection is full
    - Implement the Get method in Collection class to retrieve the elements in the collection
    - Update Program.cs to demonstrate the usage of the Collection class with integers and strings

--- ObjectOrientedProgramming/Business/Collection.cs
+++ ObjectOrientedProgramming/Business/Collection.cs
@@ -5,8 +5,30 @@ using System.Threading.Tasks;
 
 namespace ObjectOrientedProgramming.Business
 {
-    public class Collection
+    public class Collection<T>
     {
-        
+        private T[] _elements;
+        private int _index;
+        private int _limit;
+        public Collection(int limit)
+        {
+            _index = 0;
+            _limit = limit;
+            _elements = new T[_limit];
+        }
+
+        public void Add(T element)
+        {
+            if (_index < _limit)
+            {
+                _elements[_index] = element;
+                _index++;
+            }
+            else
+            {
+                throw new Exception("Collection is full");
+            }
+        }
+        public T[] Get() => _elements;
     }
 }
--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -12,3 +12,23 @@ void SendSome(ISend some)
 {
   some.Send();
 }
+
+var elements = new Collection<int>(5);
+elements.Add(1);
+elements.Add(2);
+elements.Add(3);
+elements.Add(4);
+
+foreach (var element in elements.Get())
+{
+  Console.WriteLine(element);
+}
+
+var names = new Collection<string>(5);
+names.Add("John");
+names.Add("Jane");
+names.Add("Jack");
+foreach (var name in names.Get())
+{
+  Console.WriteLine(name);
+}

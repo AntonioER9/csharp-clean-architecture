6da27d26f6db1e1ea16c50e907234c10b4a27db9
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-22 12:27:16 -0300

    Refactor solution file to include SOLID project

---
+++ SOLID/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Beer
+{
+  private List<string> _beers;
+  public Beer()
+  {
+    _beers = new List<string>();
+  }
+  public void Add(string beer)
+  {
+    _beers.Add(beer);
+  }
+  public List<string> Get()
+  {
+    return _beers;
+  }
+  public List<string> GetReport()
+  {
+    var data = new List<string>();
+    foreach (var beer in _beers)
+    {
+      data.Add("Cerveza: " + beer);
+    }
+    return data;
+  }
+  public void SaveReport(string filePath)
+  {
+    using (var writer = new StreamWriter(filePath))
+    {
+      foreach (var beer in GetReport())
+      {
+        writer.WriteLine(beer);
+      }
+    }
+  }
+}

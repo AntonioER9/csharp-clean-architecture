20b2541c39c952453b99688cbb54bc9f35fc22e0
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-24 14:21:39 -0300

    Refactor solution file to include new projects

---
+++ OperationComponent/Operations.cs
@@ -0,0 +1,6 @@
+﻿namespace OperationComponent;
+
+public class Operations
+{
+  public decimal Some(decimal a, decimal b) => a + b;
+}
---
+++ TestComponent/Program.cs
@@ -0,0 +1,6 @@
+﻿using OperationComponent;
+
+var operations = new Operations();
+var result = operations.Some(1, 2);
+Console.WriteLine(result); // 3
+

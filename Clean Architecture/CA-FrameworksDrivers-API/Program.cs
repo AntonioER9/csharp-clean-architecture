3383e4ae747243b0388f6427ac670c42d868717e
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-26 12:39:38 -0300

    Refactor solution file to include new projects and dependencies

---
+++ Clean Architecture/CA-FrameworksDrivers-API/Program.cs
@@ -0,0 +1,39 @@
+using CA_ApplicationLayer;
+using CA_InterfaceAdapters_Data;
+using CA_InterfaceAdapters_Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Dependencias
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<GetBeerUseCase>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/beer", async (GetBeerUseCase beerUseCase) =>
+{
+    return await beerUseCase.ExecuteAsync();
+})
+.WithName("beers")
+.WithOpenApi();
+
+app.Run();

33059a07cded143c8dcfacf562dfe7f1ed42e91a
AntonioER9 <aespinozareyes9@gmail.com>
2024-09-26 12:18:30 -0300

    Refactor solution file to include new projects and dependencies

--- Clean Architecture/CA-InterfaceAdapters-Repository/Repository.cs
+++ Clean Architecture/CA-InterfaceAdapters-Repository/Repository.cs
@@ -1,6 +1,28 @@
-﻿namespace CA_InterfaceAdapters_Repository;
+﻿using CA_ApplicationLayer;
+using CA_EnterpriseLayer;
+using CA_InterfaceAdapters_Data;
+using Microsoft.EntityFrameworkCore;
 
-public class Repository
+namespace CA_InterfaceAdapters_Repository;
+
+public class Repository : IRepository
 {
-
+  private readonly AppDbContext _dbContext;
+  public Repository(AppDbContext dbContext)
+  {
+    _dbContext = dbContext;
+  }
+  public async Task AddAsync(Beer beer)
+  {
+    await _dbContext.Beers.AddAsync(beer); // Add a new beer to the database
+    await _dbContext.SaveChangesAsync(); // Save changes to the database
+  }
+  public async Task<Beer> GetByIdAsync(int id)
+  {
+    return await _dbContext.Beers.FindAsync(id); // Find a beer by its id
+  }
+  public async Task<IEnumerable<Beer>> GetAllAsync()
+  {
+    return await _dbContext.Beers.ToListAsync(); // Get all beers from the database
+  }
 }
